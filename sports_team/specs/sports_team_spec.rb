1594e7d8308553550fe98598533fb2b782e54386
Scott Murray <rai.five@gmail.com>
2017-09-26 00:15:16 +0100

    Third Commit - Part B Progress

--- sports_team/specs/sports_team_spec.rb
+++ sports_team/specs/sports_team_spec.rb
@@ -21,4 +21,41 @@ def test_coach_name()
     assert_equal("Magath", team.coach_name())
 end
 
+# Create a setter method to allow the coach's name to be updated.
+
+def test_update_coach_name()
+  team = SportsTeam.new("Bayern Munich", ["Kahn", "Lahm", "Schweinsteiger", "Ballack", "Makaay"], "Magath")
+
+  team.update_coach_name("Guardiola")
+
+  assert_equal("Guardiola", team.coach_name())
+end
+
+# Create a method that adds a new player to the player's array.
+def test_add_player()
+  team = SportsTeam.new("Bayern Munich", ["Kahn", "Lahm", "Schweinsteiger", "Ballack", "Makaay"], "Magath")
+
+  team.add_player("Ribery")
+
+  assert_equal(["Kahn", "Lahm", "Schweinsteiger", "Ballack", "Makaay", "Ribery"], team.player_names())
+end
+
+# Add a method that takes in a string of a player's name and checks to see if they are in the players array.
+def test_search_player()
+    team = SportsTeam.new("Bayern Munich", ["Kahn", "Lahm", "Schweinsteiger", "Ballack", "Makaay"], "Magath")
+
+    team.search_player("Lahm")
+
+    assert_equal("Found!", team.search_player("Lahm"))
+end
+# Add a points property into your class that starts at 0.
+
+def test_points()
+
+end
+
+# Create a method that takes in whether the team has won or lost and updates the points property for a win.
+
+
+
 end
--- sports_team/sports_team.rb
+++ sports_team/sports_team.rb
@@ -18,4 +18,33 @@ def coach_name()
   return @coach
 end
 
+# Create a setter method to allow the coach's name to be updated.
+
+def update_coach_name(new_coach)
+  @coach = new_coach
+end
+
+# Create a method that adds a new player to the player's array.
+
+def add_player(new_player)
+  @players << new_player
+end
+
+# Add a method that takes in a string of a player's name and checks to see if they are in the players array.
+def search_player(player_to_search)
+  for player in @players
+    if player == player_to_search
+      return "Found!"
+    end
+  end
+  return "Not found!"
+end
+
+# Add a points property into your class that starts at 0.
+def points()
+  @points = 0
+end
+
+# Create a method that takes in whether the team has won or lost and updates the points property for a win.
+
 end
